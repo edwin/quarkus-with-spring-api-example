88b07edb7d50bac3da94643abf8cff6e9a61cd40
Edwin <edwinkun@gmail.com>
2019-12-16 17:39:53 +0700

    adding findById, testGetAll, testDeleteById unit test api

--- src/test/java/com/edw/controller/UserControllerTest.java
+++ src/test/java/com/edw/controller/UserControllerTest.java
@@ -1,10 +1,16 @@
 package com.edw.controller;
 
+import com.edw.entity.User;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.*;
@@ -22,6 +28,13 @@ import static org.hamcrest.CoreMatchers.*;
 public class UserControllerTest {
 
     @Test
+    public void testBean() {
+        User user = new User("01", "username", "password");
+        Assertions.assertNotNull(user);
+        Assertions.assertNotEquals(user, new User());
+    }
+
+    @Test
     public void testIndex() {
         given()
                 .when().get("/")
@@ -36,6 +49,14 @@ public class UserControllerTest {
                 .when().get("/404/not-found")
                 .then()
                 .statusCode(404);
+    }
+
+    @Test
+    public void testNoObjectWhenUsingRandomUid() {
+        given()
+                .when().get("/"+ UUID.randomUUID().toString())
+                .then()
+                .statusCode(204);
     }
 
     @Test
@@ -67,4 +88,75 @@ public class UserControllerTest {
                     .body(containsString("\"id\""));
     }
 
+    @Test
+    public void testFindById() {
+        Map map = given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "\t\"username\":\"666\",\n" +
+                        "\t\"passwd\":\"999\"\n" +
+                        "}")
+                .when()
+                    .post("/")
+                .then()
+                    .statusCode(200)
+                    .body(containsString("\"id\""))
+                .extract()
+                .as(HashMap.class);
+
+        String id = (String) map.get("id");
+
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                    .get("/"+id)
+                .then()
+                    .statusCode(200)
+                    .body(containsString(id));
+
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                    .get("/exist/"+id)
+                .then()
+                    .statusCode(200)
+                    .body(containsString("true"));
+    }
+
+    @Test
+    public void testGetAll() {
+        given()
+                .when()
+                    .get("/all")
+                .then()
+                    .statusCode(200);
+    }
+
+    @Test
+    public void testDeleteById() {
+        Map map = given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "\t\"username\":\"55433\",\n" +
+                        "\t\"passwd\":\"999\"\n" +
+                        "}")
+                .when()
+                .post("/")
+                .then()
+                .statusCode(200)
+                .body(containsString("\"id\""))
+                .extract()
+                .as(HashMap.class);
+
+        String id = (String) map.get("id");
+
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .delete("/"+id)
+                .then()
+                .statusCode(200);
+    }
 }
